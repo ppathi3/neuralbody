7f4958b19b6775b00ed19434a0c20f8d37e2c5a8
ppathi3 <ppathi@uncc.edu>
2023-12-13 00:33:33 -0500

    Updated app.py file

--- app.py
+++ app.py
@@ -1,26 +1,53 @@
 import streamlit as st
-import cv2
-from PIL import Image
-import os
 
-# Title and description
-st.title("Neural Body: Implicit Neural Representations with Structured Latent Codes")
-st.header("Novel View Synthesis of Dynamic Humans")
+def home_page():
+    st.title("Neural Body: Implicit Neural Representations with Structured Latent Codes")
+    st.header("Introduction to Neural Body")
+    intro_text = """
+    Our application addresses a critical challenge in synthesizing novel views of human performers from sparse camera views. 
+    While recent approaches excel with dense input views, sparse views pose an ill-posed problem for neural representation learning.
 
-# Input image path
-input_image_path = "drive/MyDrive/neuralbody/data/zju_mocap/CoreView_313/Camera (3)/CoreView_313_Camera_(3)_0544_2019-08-23_16-09-08.234.jpg"
+    To overcome this challenge, our innovation lies in integrating observations across video frames. We introduce NeuralBody, 
+    a novel representation for the human body. It assumes that learned neural representations across frames share a set of latent codes 
+    tied to a deformable mesh, allowing natural integration of observations.
 
+    This deformable mesh not only guides geometric understanding but also enables more efficient learning of 3D representations. 
+    Our approach has been evaluated on the ZJU-MoCap dataset, capturing performers with complex motions. Results demonstrate substantial 
+    improvements in novel view synthesis quality compared to prior works.
 
-# Output image and video paths
-output_image_path = "drive/MyDrive/neuralbody/data/zju_mocap/CoreView_313/Camera (3)/CoreView_313_Camera_(3)_0544_2019-08-23_16-09-08.234.jpg"
-output_video_path = "output.mp4"
+    Additionally, our approach showcases its capability to reconstruct a moving person from monocular video data on the People-Snapshot dataset.
 
-# Display input image
-st.subheader("Input Image")
-st.image(input_image_path, caption="Input Image", use_column_width=True)
+    Explore our app to delve deeper into NeuralBody's innovative techniques and its exceptional performance in view synthesis!
+    """
 
-if st.button("Process Image"):
-  st.write("3D Reconstructed Image")
-  st.image(output_image_path, caption="Output Image", use_column_width=True)
-  st.write("3D Reconstructed Video")
-  st.video(output_video_path)
+    st.write(intro_text)
+def results_page():
+    st.title("Results Page")
+    st.header("Input and Output")
+
+    # Input video selection
+    input_video = "data/people_snapshot/female-3-casual/female-3-casual.mp4"
+    st.video(input_video)
+
+    if st.button("Process"):
+
+        # Process input_video and generate output_image
+
+        # Placeholder for output paths (replace with actual paths)
+        output_image_path = "data/render/female3c/frame_0000/0000.png"
+
+        # Display output image
+        st.image(output_image_path, caption="Output Image", use_column_width=True)
+
+def main():
+    st.sidebar.title("Navigation")
+    selected_tab = st.sidebar.radio("Go to", ("Home Page", "Results Page"))
+
+    if selected_tab == "Home Page":
+        home_page()
+    elif selected_tab == "Results Page":
+        results_page()
+
+if __name__ == "__main__":
+    main()
+
